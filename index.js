9f199102a675e1c3ecd421b8f3a8c2e8bdbae9ba
Joey Palmieri <joeypalmieri@hotmail.com>
2018-07-03 01:03:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var katzDeliLine = [];
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
 
-function takeAnumber(n) {
-  
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
 }
