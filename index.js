b7011ef2ff8e3b71d5dea3aa04880ed7801d35db
Joey Palmieri <joeypalmieri@hotmail.com>
2018-07-03 01:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ function currentLine(line) {
 
   return `The line is currently: ${numbersAndNames.join(', ')}`
 }
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+}
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
